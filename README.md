5737b05996838e28e46dd2e3b7ee3d6806c3d88d
Martyn Janes <martyn@obany.com>
2017-11-10 09:00:41 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# UniteJS Test Matrix - Aurelia RequireJS
+
+Repo to run a multitude of configuration options for UniteJS on Aurelia Application Framework.
+
+[![Build Status][travis-image]][travis-url]
+
+[travis-url]: https://travis-ci.org/unitejs-test-matrix/au-requirejs-matrix/
+[travis-image]: http://img.shields.io/travis/unitejs-test-matrix/au-requirejs-matrix/master.svg?style=flat
---
+++ create-apps.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -e
+
+#
+# Aurelia SystemJS
+#
+node unitejs/cli/bin/unite configure --packageName=au-sjs-js-jas-pro --title="Aurelia SystemJS JavaScript" --sourceLanguage=JavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=PhantomJs --e2eTestRunner=Protractor --e2eTestFramework=Jasmine --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Css --cssPost=PostCss --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-pro
+
+node unitejs/cli/bin/unite configure --packageName=au-sjs-js-jas-wdr --title="Aurelia SystemJS JavaScript" --sourceLanguage=JavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=PhantomJS --e2eTestRunner=WebdriverIO --e2eTestFramework=Jasmine --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Sass --cssPost=PostCss --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-js-jas-wdr
+
+node unitejs/cli/bin/unite configure --packageName=au-sjs-js-mch-pro --title="Aurelia SystemJS JavaScript" --sourceLanguage=JavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=PhantomJS --e2eTestRunner=Protractor --e2eTestFramework=MochaChai --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Less --cssPost=PostCss --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-pro
+
+node unitejs/cli/bin/unite configure --packageName=au-sjs-js-mch-wdr --title="Aurelia SystemJS JavaScript" --sourceLanguage=JavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=PhantomJS --e2eTestRunner=WebdriverIO --e2eTestFramework=MochaChai --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Stylus --cssPost=PostCss --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-js-mch-wdr
+
+node unitejs/cli/bin/unite configure --packageName=au-sjs-ts-jas-pro --title="Aurelia SystemJS TypeScript" --sourceLanguage=TypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=PhantomJS --e2eTestRunner=Protractor --e2eTestFramework=Jasmine --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Css --cssPost=None --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-pro
+
+node unitejs/cli/bin/unite configure --packageName=au-sjs-ts-jas-wdr --title="Aurelia SystemJS TypeScript" --sourceLanguage=TypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=PhantomJS --e2eTestRunner=WebdriverIO --e2eTestFramework=Jasmine --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Sass --cssPost=None --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-jas-wdr
+
+node unitejs/cli/bin/unite configure --packageName=au-sjs-ts-mch-pro --title="Aurelia SystemJS TypeScript" --sourceLanguage=TypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=PhantomJS --e2eTestRunner=Protractor --e2eTestFramework=MochaChai --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Less --cssPost=None --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-pro
+
+node unitejs/cli/bin/unite configure --packageName=au-sjs-ts-mch-wdr --title="Aurelia SystemJS TypeScript" --sourceLanguage=TypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=PhantomJS --e2eTestRunner=WebdriverIO --e2eTestFramework=MochaChai --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Stylus --cssPost=None --appFramework=Aurelia --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=attribute --name=MyAttribute --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=binding-behavior --name=MyBindingBehavior --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=element --name=MyElement --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=pipeline-step --name=MyPipelineStep --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=value-converter --name=MyValueConverter --disableVersionCheck --outputDirectory=./apps/au-sjs-ts-mch-wdr
+
